64425d9e58b82c63006202e926136afbc8fbaabe
Jackson Blacklock <jhblacklock@twinbcreative.com>
2009-04-17 10:09:29 -0400

    added tmp auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  before_filter :authenticate
+  
+  def authenticate
+    authenticate_or_request_with_http_basic('Temporary Authentication') do |username, password|
+    htpasswd = {'aacow' => 'aacow_tmp_pass'}
+    htpasswd[username] == password
+    end
+  end
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
